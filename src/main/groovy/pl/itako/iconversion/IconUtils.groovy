e9c046a337bf6905a065fd1e2989625c7dc946db
Daniel Grech <daniel.grech@vivant.com.au>
2015-08-19 12:33:17 +1000

    Extract icon name from manifest if possible

--- src/main/groovy/pl/itako/iconversion/IconUtils.groovy
+++ src/main/groovy/pl/itako/iconversion/IconUtils.groovy
@@ -11,24 +11,60 @@ import java.util.List
 import static java.awt.RenderingHints.KEY_TEXT_ANTIALIASING
 import static java.awt.RenderingHints.VALUE_TEXT_ANTIALIAS_ON
 
+/**
+ * Various utility methods to find and draw the information on the
+ */
 class IconUtils {
 
+    static {
+        System.setProperty("awt.useSystemAAFontSettings", "on");
+        System.setProperty("swing.aatext", "true");
+    }
+
+    static final String DEFAULT_ICON_NAME = "ic_launcher";
+
+    /**
+     * Retrieve the app icon from the application manifest
+     *
+     * @param manifestFile The file pointing to the AndroidManifest
+     * @return The icon name specified in the {@code <application/ >} node
+     */
+    static String getIconName(File manifestFile) {
+        if (manifestFile == null || manifestFile.isDirectory() || !manifestFile.exists()) {
+            return null;
+        }
+
+        def manifestXml = new XmlSlurper().parse(manifestFile)
+        def fileName = manifestXml?.application?.@'android:icon'?.text()
+        return fileName == null ? null : fileName?.split("/")[1]
+    }
+
+    /**
+     * Finds all icon files matching the icon specified in the given manifest.
+     *
+     * If no icon can be found in the manifest, a default of {@link IconUtils#DEFAULT_ICON_NAME} will be used
+     */
     static List<File> findIcons(File where, File manifest) {
+        String iconName = getIconName(manifest) ?: DEFAULT_ICON_NAME
         List<File> result = Lists.newArrayList();
-        where.eachFileRecurse(FileType.FILES) { File file ->
-            //TODO use manifest for icon name
-            if (file.name == "ic_launcher.png") {
+
+        where.eachDirMatch(~/^drawable.*|^mipmap.*/) { dir ->
+            dir.eachFileMatch(FileType.FILES, ~"^${iconName}.*") { file ->
                 result.add(file)
             }
         }
 
-        result
+        return result
     }
 
+    /**
+     * Draws the given text over an image
+     *
+     * @param image The image file which will be written too
+     * @param config The configuration which controls how the overlay will appear
+     * @param lines The lines of text to be displayed
+     */
     static void addTextToImage(File image, RenderingConfig config = RenderingConfig.DEFAULT, String... lines) {
-        System.setProperty("awt.useSystemAAFontSettings","on");
-        System.setProperty("swing.aatext", "true");
-
         final BufferedImage bufferedImage = ImageIO.read(image);
 
         final Color backgroundOverlayColor = config.backgroundOverlayColor;
